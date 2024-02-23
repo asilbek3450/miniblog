b8441fea69031f2aacd73fea381419451f2b1775
asilbek <asilbekmirolimov@gmail.com>
2024-02-23 19:47:35 +0500

    changes

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
 
 from blog.forms import BlogForm
+from blog.models import BlogPost
 
 
 # Create your views here.
 def blog_list(request):
-    return render(request, 'blog_list.html')
+    bloglar = BlogPost.objects.all()
+
+    context = {
+        'blogs': bloglar
+    }
+    # https://www.bootdey.com/snippets/view/Latest-News-section
+    return render(request, 'blog_list.html', context=context)
 
 
 def blog_create(request):
@@ -24,3 +31,8 @@ def blog_create(request):
     }
 
     return render(request, 'blog_create.html', context=context)
+
+
+def blog_detail():
+    pass
+# https://www.bootdey.com/snippets/view/project-details
