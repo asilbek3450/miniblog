bcaba3c7ae65fb6cd4bedf7f4dbc4d09dd53d828
asilbek <asilbekmirolimov@gmail.com>
2024-03-01 19:34:20 +0500

    changes

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,25 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 from blog.forms import BlogForm
 from blog.models import BlogPost, BlogCategory
+from django.db.models import Q
 
 
 # Create your views here.
 def blog_list(request):
-    bloglar = BlogPost.objects.all()
     categories = BlogCategory.objects.all()
+    search_post = request.GET.get('search')
+    category = request.GET.get('category')
+    if search_post:
+        bloglar = BlogPost.objects.filter(Q(title__icontains=search_post) | Q(short_description__icontains=search_post))
+    elif category:
+        bloglar = BlogPost.objects.filter(category__name=category)
+    else:
+        bloglar = BlogPost.objects.all().order_by("-created_at")
+
     context = {
         'blogs': bloglar,
-        'categories': categories
+        'categories': categories,
+        'search_post': search_post
     }
     # https://www.bootdey.com/snippets/view/Latest-News-section
     return render(request, 'blog_list.html', context=context)
